bc77be07bc4f1d34a137617a985c60ac195b5bc9
Sakialamin01 <sakialaminwork@gmail.com>
2026-03-08 10:20:35 +0600

    Log In Page push

---
+++ login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+const error = document.getElementById("error");
+
+if(username === "admin" && password === "admin123"){
+
+localStorage.setItem("loggedIn","true");
+
+window.location.href = "dashboard.html";
+
+}else{
+
+error.classList.remove("hidden");
+
+}
+
+});
