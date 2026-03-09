677cd9ce48771c078837a9b18c312082364faa37
Sakialamin01 <sakialaminwork@gmail.com>
2026-03-09 17:14:24 +0600

    Main dash popup add

--- issue.js
+++ issue.js
@@ -6,6 +6,7 @@ const searchInput = document.getElementById("searchInput");
 let issues = [];
 let currentTab = "all";
 
+
 async function loadIssues(){
 
 const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
@@ -18,9 +19,10 @@ renderIssues();
 
 }
 
+
 function renderIssues(){
 
-container.innerHTML="";
+container.innerHTML = "";
 
 let filtered = issues;
 
@@ -44,16 +46,15 @@ filtered.forEach(issue => {
 
 const card = document.createElement("div");
 
-card.className="bg-white p-4 rounded shadow border";
+card.className =
+"bg-white p-4 rounded shadow border cursor-pointer hover:shadow-lg";
 
-card.innerHTML=`
+card.innerHTML = `
 
 <div class="flex justify-between mb-2">
-
-<span class="text-xs px-2 py-1 rounded bg-red-100 text-red-600">
-${issue.priority || "LOW"}
+<span class="text-xs px-2 py-1 rounded bg-gray-200">
+${issue.priority}
 </span>
-
 </div>
 
 <h3 class="font-semibold text-sm mb-2">
@@ -61,10 +62,10 @@ ${issue.title}
 </h3>
 
 <p class="text-xs text-gray-500 mb-3">
-${issue.description || ""}
+${issue.description.slice(0,80)}...
 </p>
 
-<div class="flex gap-2 mb-3">
+<div class="flex gap-2 mb-2">
 
 <span class="text-xs bg-red-100 text-red-600 px-2 py-1 rounded">
 BUG
@@ -82,15 +83,18 @@ HELP WANTED
 
 `;
 
+card.onclick = () => openIssue(issue.id);
+
 container.appendChild(card);
 
 });
 
 }
 
-tabs.forEach(btn=>{
 
-btn.addEventListener("click",()=>{
+tabs.forEach(btn => {
+
+btn.addEventListener("click", () => {
 
 currentTab = btn.dataset.tab;
 
@@ -108,6 +112,55 @@ renderIssues();
 
 });
 
+
 searchInput.addEventListener("input",renderIssues);
+
+
+async function openIssue(id){
+
+const res = await fetch(
+`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+);
+
+const data = await res.json();
+
+const issue = data.data;
+
+document.getElementById("modalTitle").innerText = issue.title;
+
+document.getElementById("modalDesc").innerText = issue.description;
+
+document.getElementById("modalStatus").innerText = issue.status;
+
+document.getElementById("modalAuthor").innerText =
+"Opened by " + issue.author;
+
+document.getElementById("modalDate").innerText =
+issue.createdAt;
+
+document.getElementById("modalAssignee").innerText =
+issue.assignee;
+
+document.getElementById("modalPriority").innerText =
+issue.priority;
+
+document.getElementById("modalLabel1").innerText =
+issue.labels?.[0] || "";
+
+document.getElementById("modalLabel2").innerText =
+issue.labels?.[1] || "";
+
+document.getElementById("issueModal").classList.remove("hidden");
+document.getElementById("issueModal").classList.add("flex");
+
+}
+
+
+function closeModal(){
+
+document.getElementById("issueModal").classList.add("hidden");
+
+}
+
 
 loadIssues();
